f04317ef2b8022cf3360d476c8a1243c08d0cc93
lucianojuchem <lucianojuchem@gmail.com>
2024-09-05 14:58:51 -0300

    Começo

---
+++ datanbaplayers.js
@@ -0,0 +1,33 @@
+let playersNBA = [
+    {
+        nome: "LeBron James",
+        posicao: "Ala",
+        idade: 39,
+        altura: "2,06m",
+        jogos: 1492,
+        pontos: 40474,
+        assistencias: 11009,
+        link: "https://www.nba.com/player/2544/lebron-james"
+    },
+    {
+        nome: "Stephen Curry",
+        posicao: "Armador",
+        idade: 36,
+        altura: "1,91m",
+        jogos: 956,
+        pontos: 23668,
+        assistencias: 6119,
+        link: "https://www.nba.com/player/201939/stephen-curry"
+    },
+    {
+        nome: "Kevin Durant",
+        posicao: "Ala",
+        idade: 35,
+        altura: "2,11m",
+        jogos: 1061,
+        pontos: 28924,
+        assistencias: 4645,
+        link: "https://www.nba.com/player/201142/kevin-durant"
+    },
+   
+];
